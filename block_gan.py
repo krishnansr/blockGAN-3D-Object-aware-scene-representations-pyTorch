acdec822b2c08546806d243ea0c4736686665276
Sivaramakrishnan <48220489+krishnansr@users.noreply.github.com>
2022-08-28 15:00:04 -0400

    basic gan

--- block_gan.py
+++ block_gan.py
@@ -1,17 +1,28 @@
 from torch import nn
 
 class Generator(nn.Module):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, z_dim, img_dim):
         super(Generator, self).__init__()
-        pass
+        self.gen = nn.Sequential(
+            nn.Linear(z_dim, 256),
+            nn.LeakyReLU(0.1),  # 0.01
+            nn.Linear(256, img_dim),
+            nn.Tanh(),
+        )
 
     def forward(self, x):
-        raise NotImplementedError
+        return self.gen(x)
+
 
 class Discriminator(nn.Module):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, img_dim):
         super(Discriminator, self).__init__()
-        pass
+        self.disc = nn.Sequential(
+            nn.Linear(img_dim, 128),
+            nn.LeakyReLU(0.1),  # 0.01
+            nn.Linear(128, 1),
+            nn.Sigmoid(),
+        )
 
     def forward(self, x):
-        raise NotImplementedError
+        return self.disc(x)
--- train.py
+++ train.py
@@ -9,8 +9,8 @@ import torchvision
 import torchvision.transforms as transforms
 import torch.nn.functional as F
 
+from tqdm import tqdm
 from functools import reduce
-from progressbar import progressbar
 from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
 
@@ -21,10 +21,11 @@ from .block_gan import Generator, Discriminator
 DEVICE = torch.device('cuda' if torch.cuda.is_available() else "cpu")
 print(f'using torch device: {DEVICE}, torch ver: {torch.__version__}')
 
+
 def train_model(config):
     # get hyperparams
-    lr = config.get('LR', 2e-3)
-    z_dim = config.get('Z_DIM', 64)
+    lr = config.get('LR', 2e-3)  # 3e-4
+    z_dim = config.get('Z_DIM', 64)  # 128, 256
     batch_size = config.get('BATCH_SIZE', 32)
     num_epochs = config.get('EPOCHS', 50)
     image_dim = config.get('IMAGE_DIMS', (28, 28, 1))
@@ -35,10 +36,62 @@ def train_model(config):
     fixed_noise = torch.randn((batch_size, z_dim)).to(DEVICE)
     data_transforms = transforms.Compose([
         transforms.ToTensor(),
-        transforms.Normalize((0.5,), (0.5,)),
+        transforms.Normalize((0.1307,), (0.3081,)),  # (0.5,), (0.5,)
     ])
 
+    dataset = torchvision.datasets.MNIST(root="dataset/", transform=data_transforms, download=True)
+    loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+    opt_disc = torch.optim.Adam(disc.parameters(), lr=lr)  # use SGD?
+    opt_gen = torch.optim.Adam(gen.parameters(), lr=lr)
+    criterion = torch.nn.BCELoss()  # simulate minimax eq
 
+    # tensorboard setup
+    writer_fake = SummaryWriter(f"runs/GAN_MNIST/fake")
+    writer_real = SummaryWriter(f"runs/GAN_MNIST/real")
+    global_step = 0
+    for epoch in range(num_epochs):
+        for batch_idx, (real, _) in enumerate(tqdm(loader)):
+            real = real.view(-1, image_dim).to(DEVICE)
+            batch_size = real.shape[0]
+
+            # train Discriminator: max log(D(real)) + log(1 - D(G(z)))
+            noise = torch.randn(batch_size, z_dim).to(DEVICE)  # z
+            fake = gen(noise)  # G(z)
+            disc_real = disc(real).view(-1)
+            lossD_real = criterion(disc_real, torch.ones_like(disc_real))
+            disc_fake = disc(fake).view(-1)
+            lossD_fake = criterion(disc_fake, torch.zeros_like(disc_fake))
+            lossD = (lossD_real + lossD_fake) / 2
+            disc.zero_grad()
+            lossD.backward(retain_graph=True)
+            opt_disc.step()
+
+            # train Generator: min log(1 - D(G(z))) <-> max log(D(G(z))
+            output = disc(fake).view(-1)
+            lossG = criterion(output, torch.ones_like(output))
+            gen.zero_grad()
+            lossG.backward()
+            opt_gen.step()
+
+            if batch_idx == 0:
+                print(
+                    f"Epoch [{epoch}/{num_epochs}] Batch {batch_idx}/{len(loader)} \
+                              Loss D: {lossD:.4f}, loss G: {lossG:.4f}"
+                )
+
+                with torch.no_grad():
+                    fake = gen(fixed_noise).reshape(-1, 1, 28, 28)
+                    data = real.reshape(-1, 1, 28, 28)
+                    img_grid_fake = torchvision.utils.make_grid(fake, normalize=True)
+                    img_grid_real = torchvision.utils.make_grid(data, normalize=True)
+
+                    writer_fake.add_image(
+                        "Mnist Fake Images", img_grid_fake, global_step=global_step
+                    )
+                    writer_real.add_image(
+                        "Mnist Real Images", img_grid_real, global_step=global_step
+                    )
+                    global_step += 1
 
 if __name__ == "__main__":
     config_file = ''
